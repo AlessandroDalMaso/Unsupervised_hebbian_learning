a899e80c1fc66cde36d643cfd83d5e1443ff1c84
Alessandro Dal Maso <alessandro.dalmaso2@studio.unibo.it>
2020-06-26 16:16:33 +0200

    finished the weight_infinitesimal_change function

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ sphere_radius = 1
 
 
 def product(X, Y, neuron_index):
+    # TODO: is there a better way to pass indexes?
     """Define a product for later use.
 
     To each hidden neuron is associated a different product.
@@ -40,7 +41,7 @@ def product(X, Y, neuron_index):
     return (X * product_matrix * Y)
 
 
-def learning_activation_function(neuron):  # TODO: is it the correct name?
+def learning_activation_function(neuron):  # TODO: is it the correct name? g
     """Return a value used to update the weight matrix.
 
     Implements temporal competition between patterns.
@@ -62,12 +63,38 @@ def learning_activation_function(neuron):  # TODO: is it the correct name?
     return -1*delta
 
 
-def weight_infinitesimal_change(time_scale, input_neurons, weights,
-                                weight_index):
-    """f."""
-    Q = product(input_neurons, weights) / product(weights, weights) ** \
-        (lebesgue_norm_exp - 1/lebesgue_norm_exp)
-    return 0  # TODO finish function
+def weight_infinitesimal_change(time_scale,
+                                hidden_neuron_index, inputs, weight):
+    """Calculate dW for a given weight.
+
+    Given an hidden neuron calculates dW for a single weight W associated to a
+    single visible neuron
+
+    Parameters
+    ----------
+    time-scale:
+        the time scale of the learning dynamic
+    hidden_neuron_index:
+        the index of the hidden neuron
+    inputs:
+        the value of visible neurons
+    weight:
+        the value of the weight to be updated.
+
+    Returns
+    -------
+    Float:
+        The increment of the weight, to be added to the weight itself.
+    """
+    Q = product(weights[hidden_neuron_index], inputs, hidden_neuron_index)\
+        / product(weights[hidden_neuron_index], weights[hidden_neuron_index],
+                  hidden_neuron_index)\
+        ** ((lebesgue_norm_exp-1) / lebesgue_norm_exp)
+
+    return learning_activation_function(Q)\
+        * (inputs[hidden_neuron_index] * sphere_radius ** lebesgue_norm_exp
+           - product(weights[hidden_neuron_index], inputs,
+                     hidden_neuron_index) * weight) / time_scale
 
 
 # %% Implementation
