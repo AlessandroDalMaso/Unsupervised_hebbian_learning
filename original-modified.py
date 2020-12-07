54739daa0d1e72c74a25c3c40a185563756c2ac5
Alessandro Dal Maso <alessandro.dalmaso2@studio.unibo.it>
2020-12-07 18:41:23 +0100

    fixed a bu with a missing for cycle

--- original-modified.py
+++ original-modified.py
@@ -9,20 +9,59 @@ from os.path import exists
 import CHUNeuralNetwork as chu
 
 np.random.seed(1024)
+rng = np.random.default_rng(1024)
 
 
-M = np.array(pd.read_hdf("database_file"))/255.
+# %% loading and splitting the MNIST dataset
 
-N=784 # no. of input features
-Ns = len(M)
+if not exists('./database_file'):
+    mnist, y = fetch_openml('mnist_784', version=1, return_X_y=True)
+    mnist_dataframe = pd.DataFrame(mnist)
+    mnist_dataframe.to_hdf("database_file", key="key")
+
+X_train = np.array(pd.read_hdf("database_file"))/255.
+
 
 # %%
+Ns=len(X_train)
+N = 784
+eps0=0.02    # learning rate
+Kx=10 # draw parameter
+Ky=10 # draw parameter
+hid=Kx*Ky    # number of hidden units that are displayed in Ky by Kx array
+sigma=1.0 # init weight standard deviation
+Nep=20      # number of epochs
+Num=99      # size of the minibatch
+prec=1e-30 # safety nonzero division parameter
+delta=0.4    # Strength of the anti-hebbian learning
+p=2.0        # Lebesgue norm of the weights
+k=2          # ranking parameter, must be integer that is bigger or equal than 2
+
+# %%
+
+
+synapses = np.random.normal(0, sigma, (hid, N)) # init weights
+
+for nep in range(Nep):
+    rng.shuffle(X_train)
+    for i in range(Ns//Num):
+        inputs=np.transpose(X_train[i*Num:(i+1)*Num,:])
+        my_ds = chu.plasticity_rule_vectorized(synapses, inputs.T, delta, p, 1, k,
+                                   1, activation_function=None)
+    
+        update = chu.scale_update(my_ds, nep, Nep, learn_rate=0.02)
+        
+        synapses += update
+    print(nep)
+
+
+# %% image representation
 
 def draw_weights(synapses, Kx, Ky):
     yy=0
     HM=np.zeros((28*Ky,28*Kx))
-    for y in range(Ky):
-        for x in range(Kx):
+    for y in range(Kx):
+        for x in range(Ky):
             HM[y*28:(y+1)*28,x*28:(x+1)*28]=synapses[yy,:].reshape(28,28)
             yy += 1
     plt.clf()
@@ -32,62 +71,8 @@ def draw_weights(synapses, Kx, Ky):
     plt.axis('off')
     fig.canvas.draw()
 
-# %%
 
-
-eps0=0.02    # learning rate
-Kx=10 # draw parameter
-Ky=10 # draw parameter
-hid=Kx*Ky    # number of hidden units that are displayed in Ky by Kx array
-sigma=1.0 # init weight standard deviation
-Nep=20      # number of epochs
-Num=50      # size of the minibatch
-prec=1e-30 # safety nonzero division parameter
-delta=0.4    # Strength of the anti-hebbian learning
-p=2.0        # Lebesgue norm of the weights
-k=2          # ranking parameter, must be integer that is bigger or equal than 2
-
-# %%
 
 fig=plt.figure(figsize=(12.9,10))
-
-synapses = np.random.normal(0, sigma, (hid, N)) # init weights
-for nep in range(Nep): # epoch for cycle
-
-    M=M[np.random.permutation(Ns),:] # random permutation of data
-    for i in range(Ns//Num): # batch for cycle
-        inputs=np.transpose(M[i*Num:(i+1)*Num,:]) # transposed
-        """
-        sig=np.sign(synapses)
-        tot_input = chu.hidden_neurons_func_2(inputs.T, synapses, p).T # (j,i)
-        #tot_input=np.dot(sig*np.absolute(synapses)**(p-1),inputs)
-        # product? but also activation?
-        # (100,784) dot (784,99) -> (100,99)
-
-        #y=np.argsort(tot_input, axis=0)  # argsort... of product?
-
-        #yl=np.zeros((hid,Num))  # g (j,i)
-        #yl[y[hid-1],np.arange(Num)]=1.0
-        #yl[y[hid-k],np.arange(Num)]=-delta
-        (h, a) = chu.ranker(inputs.T, synapses, None, k, p)
-        mine = np.zeros((Num, hid)) # (i,j)
-        mine[np.arange(Num), h]=1.
-        mine[np.arange(Num), a]=-0.4
-        my_yl = mine.T
-        
-        ds1 = np.dot(my_yl, np.transpose(inputs)) # minuend
-        xx=np.sum(np.multiply(my_yl,tot_input),1) # g dot <w,v>
-        ds2 = np.multiply(np.tile(xx.reshape(xx.shape[0],1),(1,N)),synapses)
-        # g dot <w,v> dot w
-        ds = ds1 - ds2 # eq. 3
-        """
-        my_ds = chu.plasticity_rule_vectorized(synapses, inputs.T, delta, p, 1, k,
-                               1, activation_function=None)
-
-        update = chu.scale_update(my_ds, nep, Nep, learn_rate=0.02)
-        
-        synapses += update
-    print(nep)
-        
-draw_weights(synapses, Kx, Ky)
+draw_weights(synapses, 10, 10)
 
