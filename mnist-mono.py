ab517ff5d3fd099cdd8b02755e38eef1e93b7c56
Alessandro Dal Maso <alessandro.dalmaso2@studio.unibo.it>
2020-12-18 14:52:29 +0100

    added main for loop

--- mnist-mono.py
+++ mnist-mono.py
@@ -1,8 +1,24 @@
 import numpy as np
 import pandas as pd
+import utilities as utils
+import CHUNeuralNetwork as chu
 np.random.seed(1024)
 
-mnist = pd.read_hdf('data/mnist', key='key')
-groups = mnist.groupby('class')
-groups.groups
-a = list(groups.__iter__())
+(X_train, y_train, X_test, y_test) = utils.mnist_loader(test_size=0.16)
+# loads 5000 samples for each figure, ordered in the array.
+
+layer1 = chu.CHUNeuralNetwork()
+epochs_per_figure=1
+batch_size=99
+
+for n in range(10):
+    indexes = np.random.permutation(5000) + n * 5000
+    data = X_train[indexes]
+    for epoch in range(epochs_per_figure):
+        for batch in utils.batchize(data, batch_size):
+            layer1.fit_single_batch(batch=batch, n_hiddens=100, delta=0.4, p=2, R=1,
+                                    scale=1, k=2, learn_rate=0.02, sigma=1,
+                                    batch_size=99, epoch=epoch,
+                                    epochs=epochs_per_figure)
+    print(n)
+    
