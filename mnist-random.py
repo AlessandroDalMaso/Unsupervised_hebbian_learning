98e29b7f851f4d95c9e84ce1e9821e75770c512c
Alessandro Dal Maso <alessandro.dalmaso2@studio.unibo.it>
2020-12-25 11:08:16 +0100

    various changes

--- mnist-random.py
+++ mnist-random.py
@@ -28,9 +28,9 @@ for epoch in range(epochs):
     batches = X.reshape((45000//batch_size, batch_size, 784))
     for batch in batches:
         layer1 = layer1.fit_single_batch(batch=batch, n_hiddens=100, delta=0.4,
-                                         p=2,
-                                         R=1, scale=1, k=2, learn_rate=0.02,
-                                         sigma=10, epoch=epoch, epochs=epochs)
+                                         p=3,
+                                         R=1, scale=1, k=5, learn_rate=0.01,
+                                         sigma=6, epoch=epoch, epochs=epochs)
     print(epoch)
 print(time()-start)
 
